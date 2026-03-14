453a14b7b533782be76895e595075ed36ac3fb77
chuongduong <duongthuychuong@gmail.com>
2026-03-14 22:24:02 +0930

    finish question 1

---
+++ question-1.py
@@ -0,0 +1,26 @@
+def check_password_strength():
+    password = input("Enter your password: ")
+    length = len(password)
+
+    has_digit = False
+    has_upper = False
+
+    for char in password:
+        if char.isdigit():
+            has_digit = True
+        if char.isupper():
+            has_upper = True
+
+    if length > 10 and has_digit and has_upper:
+        print("Strong password")
+
+    elif 6 <= length <= 10 and has_digit:
+        print("Medium password")
+
+    else:
+        print("Weak password")
+
+
+# Run the program
+if __name__ == "__main__":
+    check_password_strength()
